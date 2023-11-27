3aa9dfb5dee589d469a607c9cd2fe6b04681062c
Stefy1209 <100850162+Stefy1209@users.noreply.github.com>
2023-11-27 14:50:51 +0200

    Creates repositoryClient.py

---
+++ infrastructure/repositoryClient.py
@@ -0,0 +1,59 @@
+class ServiceClient():
+    def __init__(self, client):
+        self.__client = client
+        self.__listClient = []
+
+    def __str__(self):
+        pass
+
+    def getList(self):
+        """
+        gets client list
+        :return: list
+        """
+        return self.__listClient
+
+    def createClient(self, id, name, cnp):
+        """
+        creates a client
+        :param id: string
+        :param name: string
+        :param cnp: string
+        :return: class
+        """
+        return self.__client(id, name, cnp)
+
+    def addClient(self, client):
+        """
+        adds client to list
+        :param client: class
+        :return: -
+        """
+        self.__listClient.append(client)
+
+    def searchClientByID(self, id):
+        """
+        search client by id
+        :param id: string
+        :return: class
+        """
+        for client in self.__listClient:
+            if client.getID() == id:
+                return client
+
+    def removeClientByID(self, id):
+        """
+        removes client by id
+        :param id: string
+        :return: -
+        """
+        for client in self.__listClient:
+            if client.getID() == id:
+                self.__listClient.remove(client)
+
+    def delete(self):
+        """
+        deletes client list
+        :return: -
+        """
+        self.__listClient.clear()
